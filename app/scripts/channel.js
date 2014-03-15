de2d1a48b60316c069229ff6b9a5a0f1441e1737
Samuel Teixeira <samuelteixeiras@gmail.com>
2014-03-15 19:04:09 -0300

    Add channel file

---
+++ app/scripts/channel.js
@@ -0,0 +1,42 @@
+
+var factoryChannel  = function Channels(){
+    //membros privados
+    var _channels = [];
+    var _orderChannels = function(){
+        return _channels.sort();
+    }
+ 
+    //membros publicos
+    return {
+        addChannel: function(channel) {
+            _channels.push(channel);
+            //return this;
+        },
+        getOrderChannels: function(){
+            return _orderChannels(_channels);
+        },
+        getChannels: function(){
+        	return _channels;
+        },
+        addUploadsPlaylistId: function(channelId,playlistId){
+        	for (var i = _channels.length - 1; i >= 0; i--) {
+        		if(_channels[i].channelId == channelId){
+        			_channels[i].uploadsPlaylistId = playlistId;
+        		}
+        	};
+        }
+
+    };
+}
+
+
+
+var Channel = function (_channelId,_title,_thumbnail,_uploadsPlaylistId) {
+  
+  return {
+     channelId:  _channelId,
+     title: _title,
+     thumbnail: _thumbnail,
+     uploadsPlaylistId:_uploadsPlaylistId
+  };
+};
